0631089af62aaab7a5e79bd0c7598ef9ba4c84b7
CodeNathan <codenathan@codenathan.com>
2016-12-26 12:54:34 +0000

    moved all lists to seperate defaults

--- vgen/core/conf_validator.py
+++ vgen/core/conf_validator.py
@@ -1,3 +1,5 @@
+import defaults
+
 # current validation only on lists(tuples) and path
 
 REQUIRED = ('OPERATING_SYSTEM', 'PATH_ENABLED', 'DOCUMENT_ROOT', 'WEB_SERVER', 'WEB_SERVER_VERSION')
@@ -6,8 +8,9 @@ NOT_REQUIRED = ('DISABLE_EXT', 'PATH_DISABLE', 'DEV_ENVIRONMENT', 'ARCHIVE_DOCS'
 VARIABLE = ('WEB_SERVER_VERSION', 'DISABLE_EXT')
 PATH = ('PATH_ENABLED', 'DOCUMENT_ROOT', 'PATH_DISABLE', 'ARCHIVE_DOCS')
 
-OPERATING_SYSTEM = ('mac', 'windows', 'ubuntu', 'centos')
-WEB_SERVER = ('apache', 'ngnix')
-DEV_ENVIRONMENT = ('mamp', 'xampp', 'homestead')
+OPERATING_SYSTEM = defaults.OPERATING_SYSTEM
+WEB_SERVER = defaults.WEB_SERVER
+DEV_ENVIRONMENT = defaults.DEV_ENVIRONMENT
+
 
 
---
+++ vgen/core/defaults.py
@@ -0,0 +1,4 @@
+OPERATING_SYSTEM = ('mac', 'windows', 'ubuntu', 'centos')
+WEB_SERVER = ('apache', 'ngnix')
+DEV_ENVIRONMENT = ('mamp', 'xampp', 'homestead')
+PACKAGES = ('laravel', 'wordpress', 'codeigniter', 'cakephp', 'slim', 'symfony', 'yii2', 'zend')
