1dcc59a58a106f0d63d80825095563cb736a8266
CodeNathan <codenathan@codenathan.com>
2017-01-08 11:03:35 +0000

    updated config file to include linux version and set web dev to none

--- vgen/config.py
+++ vgen/config.py
@@ -1,6 +1,7 @@
 # Required Configurations
 
 OPERATING_SYSTEM = 'mac'  # mac | windows | ubuntu | centos
+OPERATING_SYSTEM_VERSION = False  # Only required for linux distribution
 PATH_ENABLED = '/Applications/MAMP/conf/apache/vhosts'  # Path enabled to .conf files include
 DOCUMENT_ROOT = '/Users/admin/Code'  # where all web document roots will go
 WEB_SERVER = 'apache'  # apache | ngnix
@@ -13,6 +14,6 @@ CHECK_PREREQUISITES = True
 
 DISABLE_EXT = '.bak'  # when host disabled if PATH_DISABLE is NONE all files will be suffixed with this ext
 PATH_DISABLE = None  # Path disable to .conf files
-DEV_ENVIRONMENT = 'mamp'  # None | mamp | xampp | homestead
+DEV_ENVIRONMENT = 'mamp'  # None | True | mamp | xampp | homestead | linux
 ARCHIVE_DOCS = None  # path to move archived sites to archive folder
 LOGS_PATH = '/Users/admin/Code/errors'  # required if APACHE_LOG_DIR is not defined
